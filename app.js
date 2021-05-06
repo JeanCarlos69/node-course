3f3357a0b1d05b0f76818cf6042c77bd923746e1
Jean Carlos <jeancarlossenpai@gmail.com>
2021-05-06 12:53:17 -0400

    adding the delete function

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const Blog = require('./models/blogs');
 const app = express();
 
 // data base here
-const dbURL = 'mongodb+srv://<user>:<password>@node-ninja.mhsls.mongodb.net/<>s?retryWrites=true&w=majority'
+const dbURL = 'mongodb+srv://u:p@node-ninja.mhsls.mongodb.net/s?retryWrites=true&w=majority'
 
 // {useNewUrlParser: true, useUnifiedTopology: true} to stop duplication warning
 mongoose.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -20,6 +20,7 @@ app.set('view engine', 'ejs');
 
 // middleware and static
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
 
@@ -42,17 +43,44 @@ app.get('/blogs', (req,res)=>{
         .then(result =>{
             res.render('index', { title_page: 'All blogs', blogs: result})
         })
-})
+});
+
+app.post('/blogs',(req,res) =>{
+    console.log(req.body);
+     //req.body has the same property as our object so we passed like so
+    const blog = new Blog(req.body)
+
+    blog.save()
+        .then(result =>{
+            res.redirect('/blogs') //back to the home page and he can see his blog
+        })
+        .catch(err => console.log(err))
+
+});
+
+app.get('/blogs/:id', (req,res) =>{
+    Blog.findById(req.params.id)
+        .then(result => res.render('detail', {blog: result, title_page: 'Blog Details' }))
+        .catch(err => console.log(err));
+});
+
+app.delete('/blogs/:id',(req,res) =>{
+    Blog.findByIdAndDelete(req.params.id)
+        .then(result =>{
+            res.json({redirect: '/blogs'})
+        })
+        .catch(err => console.log(err))
+});
 
 app.get('/blogs/create', (req,res) =>{
     res.render('create', {title_page: 'Create blog'});
-})
+});
 
 
 //404
 app.use((req,res) => {
     res.status(404).render('404', {title_page: '404'});
-})
+});
 
 /*
 it must be at the end, because it won't match wit our previous routing, 
